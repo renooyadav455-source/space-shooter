236f26d6f479f4df25b15e9475f465aef0358494
renooyadav455-source <renooyadav455@gmail.com>
2025-09-07 16:31:57 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,72 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
+    projectile = sprites.createProjectileFromSprite(img`
+        2 2 2 
+        2 2 2 
+        2 2 2 
+        5 5 5 
+        5 5 5 
+        5 5 2 
+        5 5 5 
+        2 5 5 
+        5 5 5 
+        5 5 2 
+        `, Space_ship, 0, -100)
+})
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    sprites.destroy(sprite, effects.fire, 500)
+    info.changeLifeBy(-1)
+    music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.InBackground)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprites.destroy(otherSprite, effects.fire, 500)
+    info.changeScoreBy(1)
+    music.play(music.melodyPlayable(music.sonar), music.PlaybackMode.UntilDone)
+})
+let Astroid: Sprite = null
+let projectile: Sprite = null
+let Space_ship: Sprite = null
+effects.starField.startScreenEffect()
+Space_ship = sprites.create(img`
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c d . . . . . . . 
+    . . . . . . . c b . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . c 6 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . . 8 6 . . . . . . . 
+    . . . . . . 8 8 9 8 . . . . . . 
+    . . . . . . 8 6 9 8 . . . . . . 
+    . . . . . c c c 8 8 8 . . . . . 
+    . . . . 8 8 6 6 6 9 8 8 . . . . 
+    . . 8 f f f c c e e f f 8 8 . . 
+    . 8 8 8 8 8 8 6 6 6 6 9 6 8 8 . 
+    8 8 8 8 8 8 8 8 6 6 6 9 6 6 8 8 
+    8 8 8 8 8 8 8 8 6 6 6 6 9 6 8 8 
+    `, SpriteKind.Player)
+Space_ship.setPosition(84, 98)
+controller.moveSprite(Space_ship)
+info.setLife(3)
+game.onUpdateInterval(500, function () {
+    Astroid = sprites.create(img`
+        . . . . . . . . c c c c . . . . 
+        . . . . c c c c c c c c c . . . 
+        . . . c f c c a a a a c a c . . 
+        . . c c f f f f a a a c a a c . 
+        . . c c a f f c a a f f f a a c 
+        . . c c a a a a b c f f f a a c 
+        . c c c c a c c b a f c a a c c 
+        c a f f c c c a b b 6 b b b c c 
+        c a f f f f c c c 6 b b b a a c 
+        c a a c f f c a 6 6 b b b a a c 
+        c c b a a a a b 6 b b a b b a . 
+        . c c b b b b b b b a c c b a . 
+        . . c c c b c c c b a a b c . . 
+        . . . . c b a c c b b b c . . . 
+        . . . . c b b a a 6 b c . . . . 
+        . . . . . . b 6 6 c c . . . . . 
+        `, SpriteKind.Enemy)
+    Astroid.setPosition(randint(0, scene.screenWidth()), 0)
+    Astroid.follow(Space_ship, 50)
+})
